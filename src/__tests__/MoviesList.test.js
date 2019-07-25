b52d53851d2fe0bc0b82d1a81f2c06a9ecf238eb
Julio Soto <jsberlanga@gmail.com>
2019-07-25 08:33:51 +0200

    add movies.results.length assertion

--- src/__tests__/MoviesList.test.js
+++ src/__tests__/MoviesList.test.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { render, fireEvent, cleanup, wait } from "react-testing-library";
+import {
+  render,
+  fireEvent,
+  cleanup,
+  wait,
+  waitForElement
+} from "react-testing-library";
 import { MemoryRouter } from "react-router-dom";
 import MoviesList from "../MoviesList";
 import { POSTER_PATH } from "../Movie";
@@ -8,24 +14,35 @@ afterEach(cleanup);
 
 global.fetch = require("jest-fetch-mock");
 
-const movie = {
-  results: [{ title: "some title", id: "stringid", poster_path: "poster.jpg" }]
+const movies = {
+  results: [
+    { title: "some title", id: "stringid", poster_path: "poster.jpg" },
+    { title: "sasdase2", id: "strinasdadgid", poster_path: "postasdasder.jpg" }
+  ]
 };
+
+const movie = movies.results[0];
 
 describe("<MoviesList />", () => {
   test("should render", async () => {
-    await fetch.mockResponseOnce(JSON.stringify(movie));
-    const { debug, getByTestId } = render(
+    await fetch.mockResponseOnce(JSON.stringify(movies));
+    const { debug, queryByTestId, getByTestId, getAllByTestId } = render(
       <MemoryRouter>
         <MoviesList />
       </MemoryRouter>
     );
     // debug();
     expect(getByTestId("loading")).toBeTruthy();
-    await wait();
+    // await wait();
+    // debug();
+    await waitForElement(() => getByTestId("movie-link"));
+
+    // We use query when we don't know whether it should be there
+    expect(queryByTestId("loading")).toBeFalsy();
     // debug();
     expect(getByTestId("movie-poster").src).toBe(
-      `${POSTER_PATH}${movie.results[0].poster_path}`
+      `${POSTER_PATH}${movie.poster_path}`
     );
+    expect(getAllByTestId("movie-link").length).toBe(movies.results.length);
   });
 });
